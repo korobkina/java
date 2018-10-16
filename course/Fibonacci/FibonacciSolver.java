b80d65e9e45c4c11654819fc56a06ea736cea30c
Анастасия Коробкина <korobkina@phystech.edu>
2018-10-16 22:06:32 +0300

    Update FibonacciSolver.java

--- course/Fibonacci/FibonacciSolver.java
+++ course/Fibonacci/FibonacciSolver.java
@@ -3,7 +3,13 @@ import static java.lang.Math.*;
 public class FibonacciSolver {
     public void calculateFibonacci(int number) {
         double ansFibonacci;
-        ansFibonacci = (pow((1 + sqrt(5))/2, number) - pow((1 - sqrt(5))/2, number))/sqrt(5);
-        System.out.println(number+" число Фибоначчи – это "+(int) ansFibonacci);
+        if (number >= 0){
+            ansFibonacci = (pow((1 + sqrt(5))/2, number) - pow((1 - sqrt(5))/2, number))/sqrt(5);
+            System.out.println(number+" число Фибоначчи – это "+(int) ansFibonacci);
+        }
+        else {
+            ansFibonacci = pow(-1, (number + 1) ) * (pow((1 + sqrt(5))/2, -number) - pow((1 - sqrt(5))/2, -number))/sqrt(5);
+            System.out.println(number+" число Фибоначчи – это "+(int) ansFibonacci);
+        }
     }
 }
