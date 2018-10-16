b895ad7e91361a0e924d3d5932bab82ddab69aac
Анастасия Коробкина <korobkina@phystech.edu>
2018-10-16 21:51:44 +0300

    Delete FizzBuzzMain.java

--- FizzBuzzMain.java
+++
@@ -1,9 +0,0 @@
-public class FizzBuzzMain {
-    public static void main(String[] args) {
-        FizzBuzzGame firstGame = new FizzBuzzGame();
-        int start = 1;
-        int end = 100;
-        firstGame.startGame(start, end);
-
-    }
-}
