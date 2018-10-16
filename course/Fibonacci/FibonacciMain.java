1f31fcebe2c3a8689ad2df5d79ef7fb6d6b153b1
Анастасия Коробкина <korobkina@phystech.edu>
2018-10-16 22:40:30 +0300

    Update FibonacciMain.java

--- course/Fibonacci/FibonacciMain.java
+++ course/Fibonacci/FibonacciMain.java
@@ -1,12 +1,15 @@
-import java.util.Scanner;
+import static java.lang.Math.*;
 
-public class FibonacciMain {
-    public static void main(String[] args) {
-        FibonacciSolver taskFibonacci = new FibonacciSolver();
-        Scanner in = new Scanner(System.in);
-        System.out.println("Введите номер желаемого числа Фибоначчи:");
-        int number = in.nextInt();
-        taskFibonacci.calculateFibonacci(number);
-        System.out.println(number+" число Фибоначчи – это "+taskFibonacci.calculateFibonacci(number));
+public class FibonacciSolver {
+    public int calculateFibonacci(int number) {
+        double ansFibonacci;
+        if (number >= 0){
+            ansFibonacci = (pow((1 + sqrt(5))/2, number) - pow((1 - sqrt(5))/2, number))/sqrt(5);
+            return (int) ansFibonacci;
+        }
+        else {
+            ansFibonacci = pow(-1, (number + 1) ) * (pow((1 + sqrt(5))/2, -number) - pow((1 - sqrt(5))/2, -number))/sqrt(5);
+            return (int) ansFibonacci;
+        }
     }
 }
