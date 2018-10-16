dbf7e3c4c1f3309600ae7832da338262d50a8ff8
Анастасия Коробкина <korobkina@phystech.edu>
2018-10-16 21:46:30 +0300

    Add files via upload

---
+++ FizzBuzzMain.java
@@ -0,0 +1,9 @@
+public class FizzBuzzMain {
+    public static void main(String[] args) {
+        FizzBuzzGame firstGame = new FizzBuzzGame();
+        int start = 1;
+        int end = 100;
+        firstGame.startGame(start, end);
+
+    }
+}
